667e6a121bce2bab3891b3eceee71a8b2ab4dc6b
Thieu Anh <lethieuanh154@gmail.com>
2025-05-28 14:38:06 +0700

    add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,14 @@ export const environment = {
   production: false,
   domainUrl: 'http://127.0.0.1:5000',
   debug: true,
-
+  firebase: {
+    apiKey: "AIzaSyA5gaPFRs3bucgL5OlNgABlmoIpn0dRgho",
+    authDomain: "anhlt67-85ac4.firebaseapp.com",
+    databaseURL: "https://anhlt67-85ac4-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "anhlt67-85ac4",
+    storageBucket: "anhlt67-85ac4.firebasestorage.app",
+    messagingSenderId: "996659001839",
+    appId: "1:996659001839:web:234b353f2cd4cc183a1e1a",
+    measurementId: "G-VYYHZH661N"
+  }
 };
